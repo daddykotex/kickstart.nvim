f27edfe9ea99b160177b6d5fd7504f3dc41c672e
David Francoeur <dfrancoeur04@gmail.com>
2024-03-07 15:00:05 -0500

    reuse the original treesitter config

--- lua/custom/plugins/nvim-treesitter.lua
+++ lua/custom/plugins/nvim-treesitter.lua
@@ -7,8 +7,9 @@ return { -- Highlight, edit, and navigate code
 
     ---@diagnostic disable-next-line: missing-fields
     require('nvim-treesitter.configs').setup {
-      ensure_installed = { 'bash', 'c', 'html', 'lua', 'markdown', 'scala', 'vim', 'vimdoc' },
+      ensure_installed = { 'bash', 'c', 'html', 'lua', 'markdown', 'vim', 'vimdoc' },
       -- Autoinstall languages that are not installed
+      auto_install = true,
       highlight = { enable = true },
       indent = { enable = true },
     }
@@ -21,4 +22,3 @@ return { -- Highlight, edit, and navigate code
     --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
   end,
 }
-
